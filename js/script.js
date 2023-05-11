5bebf1a1b1b0d86a4d5d286849eb91caea0beb08
scottmotion <scott@scottmotion.com>
2023-05-10 18:28:56 -0700

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// // document.getElementsByTagName("span").addEventListener("click", closeDetails);
+// // A $( document ).ready() block.
+// $( document ).ready(function() {
+//     document.getElementById("arrow2").addEventListener("click", closeDetails(this));
+// });
+
+
+
+// function closeDetails() {
+//     // console.log("clicked")
+//     const openEl = document.querySelectorAll('[open]')
+//     for (const el of openEl) {
+//         if ( el.id !== this.id) {
+//             el.removeAttribute("open")
+//         } else {
+//             return
+//         }
+//     }
+// }
