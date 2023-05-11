9765315866070d87afbae021caeecad26b1de5c3
scottmotion <scott@scottmotion.com>
2023-05-11 15:01:12 -0700

    Update script.js
    on button click open this panel and close all others

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 
-// $( document ).ready(function() {
-// });
+$( document ).ready(function() {
 
+  var acc = document.getElementsByClassName("accordion");     // get all accordion buttons
 
+  for (let i = 0; i < acc.length; i++) {                      // for each accordion button
+    acc[i].addEventListener("click", function() {             // add click event listener to the button
+      console.log("Clicked");
+      for (let j = 0; j < acc.length; j++) {                  // for each accordion button
+        console.log("Values: ", j)
+        if (j !== i) {                                        // if not the button that was clicked
+        acc[j].classList.remove("active");                    // remove active class
+        console.log("active removed", j, i);
+        var panel2 = acc[j].nextElementSibling;               // get this button's panel
+        panel2.style.maxHeight = null;                        // close panel
+        }
+      }
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+      this.classList.toggle("active");                        // toggle active state on button clicked
+      var panel = this.nextElementSibling;                    // get this button's panel
+      if (panel.style.maxHeight) {                            // if panel is open
+        panel.style.maxHeight = null;                         // close panel
+      } else {                                                // else pane is not open
+        panel.style.maxHeight = panel.scrollHeight + "px";    // open panel
+      } 
+    });
+  }
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
+});
