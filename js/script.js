bf12f694ec265d0898eaf2a230af165d2df8a221
scottmotion <scott@scottmotion.com>
2023-05-11 10:56:03 -0700

    change details to button
    details 'open' attribute conflicts with jquery.
    use button instead

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 
-$( document ).ready(function() {
-    const detailElements = document.getElementsByTagName("details");
-    for (const element of detailElements) {
-        element.addEventListener("click", closeDetails);
-    };
-});
+// $( document ).ready(function() {
+// });
 
 
-function closeDetails() {
-    console.log("clicked")
-};
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
